237404856fd4007f378c85a8646c9daf52fa3bf9
juanias69 <64942251+juanias69@users.noreply.github.com>
2022-08-01 10:15:42 -0500

    Se actualiza cadena

--- Data/Model/DataContext.cs
+++ Data/Model/DataContext.cs
@@ -17,7 +17,7 @@ namespace Data.Model
         protected override void OnConfiguring(DbContextOptionsBuilder builder)
         {
             //builder.UseSqlServer("Server=tcp:portalviadbcat.database.windows.net,1433;Initial Catalog=EComerce;Persist Security Info=False;User ID=Administrador;Password=Baloto2020*;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
-            builder.UseSqlServer(@"server=(LocalDB)\MSSQLLocalDB;database=EComerce;integrated security = true");
+            builder.UseSqlServer(@"server=(LocalDB)\MSSQLLocalDB;database=ECommerce;integrated security = true");
 
         }
     }
